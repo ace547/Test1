7a874dd10e5a65fe8e06fdb7dad6aef51d8289d2
ace <aakk54705@gmail.com>
2022-08-13 10:37:47 +0300

    i think i now
    hi again

--- src/main/java/com/mycompany/testgithub/TestGitHub.java
+++ src/main/java/com/mycompany/testgithub/TestGitHub.java
@@ -5,6 +5,8 @@
 
 package com.mycompany.testgithub;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ACE
@@ -14,5 +16,23 @@ public class TestGitHub {
     public static void main(String[] args) {
         System.out.println("Hi ace you boy!");
         System.out.println("Hi again!!!!!!!!!");
+         Scanner v= new Scanner (System.in);
+            int num=0;
+            int numeven=0,numodd=0;
+            
+            for (int i = 0; i<= 10; i++) {
+                System.out.println("enter 10 numbers");
+                num=v.nextInt();
+                if(num%2==0)
+                    numeven++;
+                else
+                    numodd++;
+                
+                
+
+            }
+               System.out.println("sum of all even number"+ numeven );
+                System.out.println("sum of all odd number"+ numodd );
+        
     }
 }
